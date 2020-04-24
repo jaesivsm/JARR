d1a96e27a1be4920ed487c057037834c73993a0e
François Schmidts <francois@schmidts.fr>
2020-04-24 17:40:48 +0200

    allow deletion from card

--- jsclient/src/features/clusterlist/components/SelectedObjCard.js
+++ jsclient/src/features/clusterlist/components/SelectedObjCard.js
@@ -2,10 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 // material ui  components
-import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
+import SettingsIcon from '@material-ui/icons/Build';
+import DeleteIcon from '@material-ui/icons/Delete';
 // jarr
 import { doFetchObjForEdit } from "../../editpanel/editSlice";
 import { doDeleteObj } from "../../feedlist/feedSlice";
@@ -13,16 +15,17 @@ import { doListClusters } from "../clusterSlice";
 import FeedIcon from "../../../components/FeedIcon";
 
 const mapDispatchToProps = (dispatch) => ({
-  openEditPanel(objType, objId) {
-    return dispatch(doFetchObjForEdit(objType, objId));
+  openEditPanel(id, objType) {
+    return dispatch(doFetchObjForEdit(objType, id));
   },
-  deleteFeed(id) {
-    dispatch(doDeleteObj(id, "feed"));
+  deleteObj(id, type) {
+    dispatch(doDeleteObj(id, type));
     return dispatch(doListClusters({ categoryId: "all" }));
   },
 });
 
-function SelectedObjCard({ id, str, type, iconUrl, errorCount, lastRetrieved, openEditPanel }) {
+function SelectedObjCard({ id, str, type, iconUrl, errorCount, lastRetrieved,
+                           openEditPanel, deleteObj }) {
   const objType = type === "feed" ? "feed" : "category";
   return (
     <Card variant="outlined">
@@ -31,11 +34,16 @@ function SelectedObjCard({ id, str, type, iconUrl, errorCount, lastRetrieved, op
         {str}
       </CardContent>
       <CardActions>
-        <Button size="small"
-          onClick={() => openEditPanel(objType, id)}
+        <IconButton size="small"
+          onClick={() => openEditPanel(id, objType)}
       >
-          Edit {objType}
-        </Button>
+         <SettingsIcon size="small" />
+        </IconButton>
+        <IconButton size="small"
+          onClick={() => deleteObj(id, objType)}
+      >
+         <DeleteIcon size="small" />
+        </IconButton>
       </CardActions>
     </Card>
    );
@@ -49,6 +57,7 @@ SelectedObjCard.propTypes = {
   errorCount: PropTypes.number,
   lastRetrieved: PropTypes.string,
   openEditPanel: PropTypes.func.isRequired,
+  deleteObj: PropTypes.func.isRequired,
 };
 
 export default connect(null, mapDispatchToProps)(SelectedObjCard);
