36fb9dd26903315ae895455c9e046fbf481145aa
François Schmidts <francois@schmidts.fr>
2020-05-01 13:50:41 +0200

    burger menu in topmenu for small display

--- jsclient/src/features/topmenu/TopMenu.js
+++ jsclient/src/features/topmenu/TopMenu.js
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import clsx from "clsx";
 // material components
+import { useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Tooltip from "@material-ui/core/Tooltip";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
+import Menu from "@material-ui/core/Menu";
 // material icons
 import MenuIcon from "@material-ui/icons/Menu";
+import MenuOpenIcon from '@material-ui/icons/MenuOpen';
 import FilterAllOrFavoriteIcon from "@material-ui/icons/StarBorder";
 import FilterFavoriteIcon from "@material-ui/icons/Star";
 import FilterAllIcon from "@material-ui/icons/IndeterminateCheckBox";
 import FilterUnreadIcon from "@material-ui/icons/CheckBoxOutlineBlank";
 import MarkAllAsReadIcon from "@material-ui/icons/LibraryAddCheck";
-import MarkALlNonClusterAsReadIcon from "@material-ui/icons/PlaylistAddCheck";
+import MarkAllNonClusterAsReadIcon from "@material-ui/icons/PlaylistAddCheck";
 import SettingsIcon from "@material-ui/icons/Settings";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 // jarr
+import TopMenuCommand from "./components/TopMenuCommand";
 import topMenuStyle from "./topMenuStyle";
 import { doLogout } from "../../authSlice";
 import { toggleMenu, doMarkAllAsRead } from "../feedlist/feedSlice";
@@ -53,8 +58,101 @@ const mapDispatchToProps = (dispatch) => ({
   },
 });
 
+
 function TopMenu(props) {
+  const theme = useTheme();
   const classes = topMenuStyle();
+  const [showMenu, setShowMenu] = useState(false);
+  const burgered = !useMediaQuery(theme.breakpoints.up("md"));
+  let menu;
+  const getLabel = (key) => {
+    if ((key === "unread" && props.isFilteringOnAll)
+         || (key === "liked" && props.isFilteringOnLiked)) {
+      return "Show all";
+    }
+    return { drawer: "Show feed list",
+             unread: "Show unread articles",
+             liked: "Show only liked articles",
+             mark: "Mark all as read",
+             markNC: "Mark all non cluster as read",
+    }[key];
+  };
+
+  const filterReadButton = (
+    <TopMenuCommand burgered={burgered} setShowMenu={setShowMenu}
+      onClick={() => props.filterClusters(props.isFilteringOnAll ? null : "all" )}
+      icon={props.isFilteringOnAll ? <FilterAllIcon />: <FilterUnreadIcon />}
+      title={getLabel("unread")} />
+  );
+  const filterLikedButton = (
+    <TopMenuCommand burgered={burgered} setShowMenu={setShowMenu}
+      onClick={() => props.filterClusters(props.isFilteringOnLiked ? null : "liked" )}
+      icon={props.isFilteringOnLiked ? <FilterFavoriteIcon />: <FilterAllOrFavoriteIcon />}
+      title={getLabel("liked")} />
+  );
+  const markAllAsReadButton = (
+    <TopMenuCommand burgered={burgered} setShowMenu={setShowMenu}
+      onClick={() => props.markAllAsRead(false)}
+      icon={<MarkAllAsReadIcon />} title={getLabel("mark")}
+    />
+  );
+  const markAllNonClusterAsReadButton = (
+    <TopMenuCommand burgered={burgered} setShowMenu={setShowMenu}
+      onClick={() => props.markAllAsRead(true)}
+      icon={<MarkAllNonClusterAsReadIcon />} title={getLabel("markNC")}
+    />
+  );
+  const showFeedList = (
+    <Tooltip title="Show feed list">
+      <IconButton
+        color="inherit"
+        aria-label="open drawer"
+        onClick={props.toggleFeedList}
+        edge="start"
+        className={clsx(classes.menuButton,
+                        props.isMenuButtonHidden && classes.hide)}>
+        <MenuIcon />
+      </IconButton>
+    </Tooltip>
+  );
+
+  if (burgered) {
+    const openMenuIcon = (<IconButton
+          color="inherit"
+          onClick={() => setShowMenu(!showMenu)}
+        >
+          <MenuOpenIcon />
+        </IconButton>);
+
+    menu = (
+      <div>
+        {showFeedList}
+        {openMenuIcon}
+        <Menu
+          id="simple-menu"
+          anchorEl={openMenuIcon}
+          keepMounted
+          open={showMenu}
+          onClose={() => setShowMenu(false)}
+        >
+          {filterReadButton}
+          {filterLikedButton}
+          {markAllAsReadButton}
+          {markAllNonClusterAsReadButton}
+        </Menu>
+      </div>
+    );
+  } else {
+    menu = (
+      <div>
+        {showFeedList}
+        {filterReadButton}
+        {filterLikedButton}
+        {markAllAsReadButton}
+        {markAllNonClusterAsReadButton}
+      </div>
+    );
+  }
 
   const className = clsx(classes.appBar, {
     [classes.appBarShift]: props.isShifted,
@@ -62,55 +160,7 @@ function TopMenu(props) {
   return (
     <AppBar position="fixed" className={className}>
       <Toolbar className={clsx(classes.toolbar)}>
-        <div>
-        <Tooltip title="Show feed list">
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={props.toggleFeedList}
-            edge="start"
-            className={clsx(classes.menuButton, props.isMenuButtonHidden && classes.hide)}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Tooltip>
-        <Tooltip title={props.isFilteringOnAll ? "Show unread articles" : "Show all"}>
-          <IconButton
-            color="inherit"
-            onClick={() => props.filterClusters(props.isFilteringOnAll ? null : "all" )}
-            className={clsx(classes.menuButton)}
-          >
-            {props.isFilteringOnAll ? <FilterAllIcon /> : <FilterUnreadIcon />}
-          </IconButton>
-        </Tooltip>
-        <Tooltip title={props.isFilteringOnLiked ? "Show all" : "Show only liked articles"}>
-          <IconButton
-            color="inherit"
-            onClick={() => props.filterClusters(props.isFilteringOnLiked ? null : "liked" )}
-            className={clsx(classes.menuButton)}
-          >
-            {props.isFilteringOnLiked ? <FilterFavoriteIcon /> : <FilterAllOrFavoriteIcon />}
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="Mark all as read">
-          <IconButton
-            color="inherit"
-            onClick={() => props.markAllAsRead(false)}
-            className={clsx(classes.menuButton)}
-          >
-            <MarkAllAsReadIcon />
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="Mark all non cluster as read">
-          <IconButton
-            color="inherit"
-            onClick={() => props.markAllAsRead(true)}
-            className={clsx(classes.menuButton)}
-          >
-            <MarkALlNonClusterAsReadIcon />
-          </IconButton>
-        </Tooltip>
-        </div>
+        {menu}
         <Typography>JARR</Typography>
         <div>
           <Tooltip title="Settings">
