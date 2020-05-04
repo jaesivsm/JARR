a93e3ebb6960e3145f8b0192ec78f7683cbe2c87
François Schmidts <francois@schmidts.fr>
2020-05-04 21:34:56 +0200

    fix

--- ep_celery.py
+++ ep_celery.py
@@ -12,6 +12,7 @@ celery_app = Celery(broker=conf.celery.broker_url,
 signals.task_success.connect(commit_pending_sql)
 signals.task_failure.connect(rollback_pending_sql)
 
-app = make_wsgi_app()
-httpd = make_server('', conf.worker.metrics.port, app)
-httpd.serve_forever()
+if not conf.jarr_testing:  # blocks CI otherwise
+    app = make_wsgi_app()
+    httpd = make_server('', conf.worker.metrics.port, app)
+    httpd.serve_forever()
