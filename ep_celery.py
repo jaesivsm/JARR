c22ab158959d9175c35ad879c9416b7b69d5a9c6
François Schmidts <francois@schmidts.fr>
2020-05-04 10:51:39 +0200

    Worker: fixing worker basic imports

--- ep_celery.py
+++ ep_celery.py
@@ -3,6 +3,7 @@
 from celery import Celery, signals
 from jarr.bootstrap import conf, commit_pending_sql, rollback_pending_sql
 
-celery_app = Celery(broker=conf.celery.BROKER_URL, config_source=conf.celery)
+celery_app = Celery(broker=conf.celery.BROKER_URL,
+                    config_source=conf.celery)
 signals.task_success.connect(commit_pending_sql)
 signals.task_failure.connect(rollback_pending_sql)
