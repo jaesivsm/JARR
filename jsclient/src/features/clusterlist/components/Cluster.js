52fe8f19700a610be32eeba8df37d40f7ab24e2f
François Schmidts <francois@schmidts.fr>
2020-05-04 11:20:31 +0200

    FIX: actually marking read reason on marked / consulted

--- jsclient/src/features/clusterlist/components/Cluster.js
+++ jsclient/src/features/clusterlist/components/Cluster.js
@@ -16,7 +16,6 @@ import LikedIcon from "@material-ui/icons/Star";
 import LikedIconBorder from "@material-ui/icons/StarBorder";
 // jarr
 import { doFetchCluster, doEditCluster, removeClusterSelection,
-         updateClusterAttrs,
 } from "../clusterSlice";
 import makeStyles from "./style";
 import { changeReadCount } from "../../feedlist/feedSlice";
@@ -75,8 +74,8 @@ const mapDispatchToProps = (dispatch) => ({
   },
   readOnRedirect(e, cluster) {
     e.stopPropagation();
-    dispatch(updateClusterAttrs({
-      clusterId: cluster.id, read: true }));
+    dispatch(doEditCluster(cluster.id,
+                           { read: false, "read_reason": "consulted" }));
     return dispatch(changeReadCount({
       feedsId: cluster["feeds_id"],
       categoriesId: cluster["categories_id"],
