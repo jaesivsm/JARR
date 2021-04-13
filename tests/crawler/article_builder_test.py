6a41d818c3ae9e91f21d15d6bcb853d29e594b9c
François Schmidts <francois@schmidts.fr>
2021-04-13 11:18:05 +0200

    adding thouroug testing to new enclosure feature (#163)

---
+++ tests/crawler/article_builder_test.py
@@ -0,0 +1,32 @@
+import json
+
+from jarr.controllers import (ArticleController, ClusterController,
+                              FeedController, UserController)
+from jarr.crawler.article_builders.classic import ClassicArticleBuilder
+from tests.base import BaseJarrTest
+
+
+class CrawlerMainTest(BaseJarrTest):
+
+    @property
+    def entry_w_enclosure(self):
+        with open('tests/fixtures/entry-with-enclosure.json') as fd:
+            return json.load(fd)
+
+    def test_articles_with_enclosure(self):
+        feed = FeedController().read().first()
+        UserController().update({'id': feed.user_id},
+                                {'cluster_enabled': True})
+        builder = ClassicArticleBuilder(feed, self.entry_w_enclosure)
+        self.assertIsNone(builder.article.get('article_type'))
+        raw_articles = list(builder.enhance())
+        self.assertEqual(2, len(raw_articles))
+        self.assertEqual('audio', raw_articles[1]['article_type'].value)
+        articles = []
+        for raw_article in raw_articles:
+            articles.append(
+                ArticleController(feed.user_id).create(**raw_article))
+        ClusterController(feed.user_id).clusterize_pending_articles()
+        a1 = ArticleController().get(id=articles[0].id)
+        a2 = ArticleController().get(id=articles[1].id)
+        self.assertEqual(a1.cluster_id, a2.cluster_id)
