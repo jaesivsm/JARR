274012668659b6850281d6836005bbba202a2992
François Schmidts <francois@schmidts.fr>
2021-04-16 15:09:50 +0200

    wip

--- jarr/lib/const.py
+++ jarr/lib/const.py
@@ -1,12 +1,13 @@
 from datetime import datetime, timezone
 
 UNIX_START = datetime(1970, 1, 1, tzinfo=timezone.utc)
-MIMETYPES = (('application/atom+xml', 1),
-             ('application/rss+xml', 0.9),
-             ('application/rdf+xml', 0.8),
-             ('application/xml', 0.5),
-             ('text/xml', 0.5),
-             ('application/json', 1),
+MIMETYPES = (('application/feed+json', 1),
+             ('application/atom+xml', 1),
+             ('application/rss+xml', 1),
+             ('application/rdf+xml', 0.9),
+             ('application/xml', 0.8),
+             ('application/json', 0.8),
+             ('text/xml', 0.6),
              ('*/*', 0.2))
 
 FEED_ACCEPT_HEADERS = ','.join(mtype + (';q=%0.1f' % qual if qual < 1 else '')
