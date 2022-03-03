87a69750387d5e25992dc5f22185b92e8e23888a
François Schmidts <francois@schmidts.fr>
2022-03-03 17:12:57 +0100

    wip

--- jarr/metrics.py
+++ jarr/metrics.py
@@ -8,45 +8,40 @@ BUCKETS_7D = [615, 922, 1383, 2073, 3109, 4663, 6994, 10490, 15734, 23600,
               35399, 53098, 79646, 119468, 179201, 268801, 403200, 604798]
 
 
-READ = Counter('read', 'Read event', ['reason'], namespace='jarr',
-               registry=REGISTRY)
+def prom(metric_cls, *args, **kwargs):
+    return metric_cls(*args, namespace='jarr', registry=REGISTRY, **kwargs)
 
-FEED_FETCH = Counter('feed_fetch', 'Feed fetching event',
-                     ['feed_type', 'result'], namespace='jarr',
-                     registry=REGISTRY)
 
-FEED_LATENESS = Histogram('feed_lateness',
-                          'observed delta time when fetching feed',
-                          ['feed_type'],
-                          buckets=BUCKETS_7D,
-                          namespace='jarr', registry=REGISTRY)
+READ = prom(Counter, 'read', 'Read event', ['reason'])
 
-FEED_EXPIRES = Histogram('feed_expires',
-                         'detlta time in second observed when setting expires',
-                         ['feed_type', 'method'], buckets=BUCKETS_7D,
-                         namespace='jarr', registry=REGISTRY)
+FEED_FETCH = prom(Counter, 'feed_fetch', 'Feed fetching event',
+                  ['feed_type', 'result'])
 
-WORKER_BATCH = Histogram('worker_batch', 'worker batch size', ['worker_type'],
-                         buckets=BUCKETS_3H[:-7], namespace='jarr',
-                         registry=REGISTRY)
+FEED_LATENESS = prom(Histogram, 'feed_lateness',
+                     'observed delta time when fetching feed',
+                     ['feed_type'], buckets=BUCKETS_7D)
 
-TFIDF_SCORE = Histogram('tfidf_score', 'TFIDF scores', ['feed_type'],
-                        buckets=[i / 100 for i in range(0, 100, 10)],
-                        namespace='jarr', registry=REGISTRY)
+FEED_EXPIRES = prom(Histogram, 'feed_expires',
+                    'detlta time in second observed when setting expires',
+                    ['feed_type', 'method'], buckets=BUCKETS_7D)
 
-WORKER = Histogram(
-        'worker_method', 'worker taken actions', ['method'],
-        buckets=BUCKETS_3H, namespace='jarr', registry=REGISTRY)
+WORKER_BATCH = prom(Histogram, 'worker_batch', 'worker batch size',
+                    ['worker_type'], buckets=BUCKETS_3H[:-7])
 
-EVENT = Counter('event', 'events', ['module', 'context', 'result'],
-                namespace='jarr', registry=REGISTRY)
+TFIDF_SCORE = prom(Histogram, 'tfidf_score', 'TFIDF scores', ['feed_type'],
+                   buckets=[i / 100 for i in range(0, 100, 10)])
 
-ARTICLE_CREATION = Counter('article_creation', 'Article Creation',
-                           ['read', 'read_reason', 'cluster'],
-                           namespace='jarr', registry=REGISTRY)
+WORKER = prom(Histogram, 'worker_method', 'worker taken actions',
+              ['method', 'result'], buckets=BUCKETS_3H)
 
-SERVER = Counter('server_method', 'server taken actions',
-                 ['uri', 'method', 'result'], namespace='jarr',
-                 registry=REGISTRY)
+EVENT = prom(Counter, 'event', 'events', ['module', 'context', 'result'])
 
-USER = Gauge('users', 'users', ['status'], namespace='jarr', registry=REGISTRY)
+ARTICLE_CREATION = prom(Counter, 'article_creation', 'Article Creation',
+                        ['read', 'read_reason', 'cluster'])
+
+SERVER = prom(Counter, 'server_method', 'HTTP method served',
+              ['uri', 'method', 'result'])
+
+USER = prom(Gauge, 'users', 'User counts', ['status'])
+
+ARTICLES = prom(Gauge, 'articles', 'Article counts', ['status'])
