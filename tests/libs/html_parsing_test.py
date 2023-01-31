55ed23f776e43709840b3c1eeb5c0b13ec308426
François Schmidts <francois@schmidts.fr>
2023-01-31 19:17:04 +0100

    Enhancing feed building from youtube url
    * fix: won't break when xml feed is directly handed to the feed builder
    * support youtube playlist:
        * supporting direct xml feed handling
        * building from playlist page
        * building from video url in a plalist
    * while still supporting bad SSL, won't try unsecure request each time

--- tests/libs/html_parsing_test.py
+++ tests/libs/html_parsing_test.py
@@ -22,7 +22,7 @@ class HTMLParsingTest(unittest.TestCase):
     @property
     def article2(self):
         resp = Response()
-        resp.url = 'https://www.youtube.com/watch?v=scbrjaqM3Oc'
+        resp.url = 'https://www.youtube.com/@BenzaieLive2'
         resp.encoding = 'utf8'
         with open('tests/fixtures/article-2.html', 'rb') as fd:
             setattr(resp, '_content', fd.read())
@@ -31,15 +31,13 @@ class HTMLParsingTest(unittest.TestCase):
     def test_extract_title(self):
         self.assertEqual('Les plus belles boulangeries de Paris',
                          extract_title(self.article))
-        self.assertEqual("Ceci n'est pas Old Boy - Owlboy (suite) "
-                         "- Benzaie Live", extract_title(self.article2))
+        self.assertEqual("BenzaieLive", extract_title(self.article2))
 
     def test_extract_feed_links(self):
         self.assertEqual(self.article.url + '/feed',
                          list(extract_feed_links(self.article))[0])
-
-        yt_feed_link = 'http://www.youtube.com/oembed?url=https%3A%2F%2F'\
-                       'www.youtube.com%2Fwatch%3Fv%3DscbrjaqM3Oc&format=json'
+        yt_feed_link = ('https://www.youtube.com/feeds/videos.xml'
+                        '?channel_id=UCOWsWZTiXkbvQvtWO9RA0gA')
         self.assertEqual(yt_feed_link,
                          list(extract_feed_links(self.article2))[0])
 
@@ -51,5 +49,6 @@ class HTMLParsingTest(unittest.TestCase):
         self.assertEqual('http://www.pariszigzag.fr/wp-content/themes'
                          '/paris_zigzag_2016/favicon.ico',
                          extract_icon_url(self.article))
-        self.assertEqual('https://s.ytimg.com/yts/img/favicon-vflz7uhzw.ico',
-                         extract_icon_url(self.article2))
+        self.assertEqual(
+            'https://www.youtube.com/s/desktop/d8e1215c/img/favicon.ico',
+            extract_icon_url(self.article2))
